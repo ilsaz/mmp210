35b33cd83ec7d0b3d6e2df32d60b3f4b79c593ae
ilsaz <ilsalaz79@gmail.com>
2019-09-17 15:23:00 -0400

    Initial commit

---
+++ variables/sketch.js
@@ -0,0 +1,50 @@
+/*variables*/
+var faceSize=275;
+x=300;
+y=200;
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setup(){
+    createCanvas(600, 400);
+}
+function draw(){
+    background("lightblue"); 
+    fill("PINK")
+    noStroke();
+    ellipse(faceSize, faceSize, faceSize);
+    fill("GREEN")
+    rect(x,y,70, 70);
+    fill("GREEN") 
+    rect(200,120, 70, 70);
+    fill("WHITE")
+    ellipse(354,150, 20);
+    ellipse(220,150, 20);
+    fill("BLACK")
+    circle(315,220,50)
+    fill("#2727ff")
+    ellipse(185,30,70,325);
+    ellipse(450,20,70,320);
+    rect(65,65, 400, 40);
+
+    
+    
+    
+    
+    
+    
+    
+    // points
+    point(100,10);
+    line(20, 20, 180, 20);
+}
